23eb319d2b772f21789458b8be4fca8c05da3206
ShanaySharif <Shanay.uw17@gmail.com>
2023-07-15 14:22:53 -0400

    pass test for cost three

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -2,6 +2,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Bakery.Models;
 using System.Collections.Generic;
 using System;
+//add   passing test for new instance of the Bread class w/qty of 2
 
 namespace Bakery.Tests
 {
@@ -18,21 +19,29 @@ namespace Bakery.Tests
         }
 
         [TestMethod]
-        public void costOfBread_ReturnsTheCostOfOneLoaf_Bread()
+        public void costOfBread_ReturnsTheCostOfOneLoafof_Bread()
         {
             Bread newBread = new Bread(1);
             Assert.AreEqual(5, newBread.BreadCost());
         }
 
         [TestMethod]
-        public void costOfBread_ReturnTheCostOfTwoLoaves_Bread()
+        public void costOfBread_ReturnTheCostOfTwoLoavesof_Bread()
         {
             Bread newBread = new Bread(2);
-            Assert.AreEqual(10,newBread.BreadCost());
+            Assert.AreEqual(10, newBread.BreadCost());
+        }
 
+        [TestMethod] //#3
+        public void costOfBread_ReturnTheCostOfThreeLoavesof_Bread()
+        {
+            Bread newBread = new Bread(3);
+            Assert.AreEqual(10, newBread.BreadCost());
 
         }
+        
 
     }
 
 }
+
